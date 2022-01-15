33ddf713982625a0d5be6d36e14d425abd558514
sako-is <isazadasaid@gmail.com>
2022-01-14 21:29:32 -0500

    oops forgot to comment out some stuff

--- main.js
+++ main.js
@@ -15,8 +15,15 @@ camera.position.setZ(30);
 renderer.render(camera, scene);
 
 const geometry = new THREE.TorusGeometry(10, 3, 16, 100);
-const material = new THREE.MeshBasicMaterial({color: 0xf37112, wireframe: true});
+const material = new THREE.MeshStandardMaterial({color: 0xf37112, wireframe: true});
 const torus = new THREE.Mesh(geometry, material);
+
+const pointLight = new THREE.PointLight(0xffffff);
+pointLight.position.set(5, 5, 5);
+
+const ambientLight = new THREE.AmbientLight(0xffffff);
+scene.add(pointLight, ambientLight);
+
 
 
 scene.add(torus);
@@ -28,9 +35,9 @@ function animate() {
   torus.rotation.y += 0.005;
   torus.rotation.z += 0.01;
 
-  torus.scale.x += 0.01;
-  torus.scale.y += 0.005;
-  torus.scale.z += 0.01;
+  torus.scale.x += 0.001;
+  torus.scale.y += 0.0005;
+  torus.scale.z += 0.001;
   renderer.render(scene, camera);
 }
 
